26262f30e2f99912df8de1dd7db1856511c05637
itnwork <289702297@qq.com>
2021-09-13 12:27:34 +0800

    add content

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,108 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from '@vuepress/theme-default'
+import { navbar, sidebar } from './configs'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  base: '/',
+
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: `/images/icons/favicon-16x16.png`,
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: `/images/icons/favicon-32x32.png`,
+      },
+    ],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['meta', { name: 'application-name', content: 'VuePress' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'VuePress' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon.png` },
+    ],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/images/icons/safari-pinned-tab.svg',
+        color: '#3eaf7c',
+      },
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  ],
+
+  // site-level locales config
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: '艾特学苑',
+      description: 'Vue 驱动的静态网站生成器',
+    },
+  },
+
+  bundler:
+    // specify bundler via environment variable
+    '@vuepress/webpack',
+
+  themeConfig: {
+    logo: '/images/hero.png',
+
+    repo: 'vuepress/vuepress-next',
+
+    docsDir: 'docs',
+
+    // theme-level locales config
+    locales: {
+      /**
+       * Chinese locale config
+       */
+      '/': {
+        // navbar
+        navbar: navbar.zh,
+
+        // sidebar
+        sidebar: sidebar.zh,
+
+        // page meta
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdatedText: '上次更新',
+        contributorsText: '贡献者',
+
+        // custom containers
+        tip: '提示',
+        warning: '注意',
+        danger: '警告',
+
+        // 404 page
+        notFound: [
+          '这里什么都没有',
+          '我们怎么到这来了？',
+          '这是一个 404 页面',
+          '看起来我们进入了错误的链接',
+        ],
+        backToHome: '返回首页',
+
+        // a11y
+        openInNewWindow: '在新窗口打开',
+        toggleDarkMode: '切换夜间模式',
+        toggleSidebar: '切换侧边栏',
+      },
+    },
+  },
+})
